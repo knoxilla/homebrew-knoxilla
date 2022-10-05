b7ded7a138dfd34fb61bc429a3d354a4159d87ad
Thomas Knox <tknox@umich.edu>
2022-10-05 15:02:16 -0400

    Update devops-calendar-update formula with descr & url

--- devops-calendar-update.rb
+++ devops-calendar-update.rb
@@ -1,11 +1,9 @@
 class DevopsCalendarUpdate < Formula
-  desc ""
-  homepage ""
+  desc "A very particular script that take JSON input and invokes a custom AWS Lambda function to post that JSON to a Google calendar shared by our DevOps team"
+  homepage "https://github.com/knoxilla/handy-scripts"
   url "https://github.com/knoxilla/handy-scripts/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "ffab5d3afd7be7960e8ddb8d8c68cad23089680c2c8c9cb9ba0fb92ea5307295"
   license ""
-
-  depends_on "awscli"
 
   def install
     bin.install 'bin/devops-calendar-update'
